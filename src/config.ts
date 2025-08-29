040c3a06567802fe41173643d79d9e3b3c1e46c6
subl-1me <julio.ugalde404@gmail.com>
2025-08-29 17:04:09 -0600

    Clodinary - packages & config setup

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,6 @@ export const API_URL = process.env.API_URL || `http://localhost:${PORT}/api`;
 export const DB_URL =
   process.env.DB_URL || "mongodb://localhost:27017/mydatabase";
 export const MONGO_URL = process.env.MONGO_URL || "";
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "";
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "";
